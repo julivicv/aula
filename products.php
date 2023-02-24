ef2f485ac1cbc3e52fd83fdfa97184159540797f
julivicv <102142918+julivicv@users.noreply.github.com>
2023-02-24 11:05:35 -0300

    Constructed the product page

--- products.php
+++ products.php
@@ -4,8 +4,28 @@ require("vendor/autoload.php");
 $loader = new \Twig\Loader\FilesystemLoader("./templates");
 $twig = new \Twig\Environment($loader);
 
-$tamplete = $twig->load('products.html');
+$template = $twig->load('products.html');
 
-echo $tamplete->render([
+$products = [
+    [
+        'name' => 'Back Scratcher',
+        'price' => 29.99,
+    ],
+    [
+        'name' => 'Racing Kart',
+        'price' => 1299.99,
+    ],
+    [
+        'name' => 'Toilet Paper',
+        'price' => 3.99,
+    ],
+    [
+        'name' => 'Shopping Cart',
+        'price' => 299.99,
+    ]
+];
+
+echo $template->render([
     'title' => 'Product',
+    'products' => $products
 ]);
